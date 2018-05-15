07589d65daa41b57a5161179b6bf2ab18b838d5b
Rafael Brier <rafaelmjf2010@gmail.com>
2018-05-15 01:42:24 -0300

    SomeChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginTabPage } from '../pages/login-tab/login-tab';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { HomePage } from '../pages/home/home';
+import { DatabaseServiceProvider } from '../providers/auth/database-service';
+import { MyServicesProvider } from '../providers/my-services/my-services';
+import { UserServiceProvider } from '../providers/auth/user-service';
 
 
 @Component({
@@ -12,13 +15,30 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
 
-  rootPage: any;
+  rootPage: any;  
+  imgUrl: string;
+  displayName: string;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth, keyboard: Keyboard) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, afAuth: AngularFireAuth, keyboard: Keyboard,
+    private databaseService: DatabaseServiceProvider,
+    private myServices: MyServicesProvider,
+    private userServices: UserServiceProvider) {
+
     const authObserver = afAuth.authState.subscribe(user => {
       if (user) {
         this.rootPage = HomePage;
-        authObserver.unsubscribe();
+
+        const userDataObserver = this.databaseService.readDatabase(user.uid)
+        .subscribe((resUser: any) => { 
+          this.displayName = resUser.name;         
+          this.imgUrl = resUser.imgUrl;
+
+          authObserver.unsubscribe();
+          userDataObserver.unsubscribe();
+        }, error => {
+          let toast = this.myServices.criarToast('Não foi possível acessar o banco de dados.');
+          toast.present();
+        });
       } else {
         this.rootPage = LoginTabPage;
         authObserver.unsubscribe();
@@ -32,6 +52,11 @@ export class MyApp {
       statusBar.backgroundColorByHexString("#d0a61c");      
       splashScreen.hide();
     });
+  }
+
+  
+  changeProfileImg(){
+    this.userServices.changeProfileImg();
   }
 }
 
